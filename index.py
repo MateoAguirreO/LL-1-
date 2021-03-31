363dc9d9eeb1e0cff5f6193a9fe6cbf0b11c48cd
Mateo A <mateoaguirre67@gmail.com>
2021-03-31 12:55:16 -0500

    grammar y metodos sin implementar

--- index.py
+++ index.py
@@ -1,1 +1,34 @@
-//code 
+import ll1
+
+grammar = {
+    1: 'S -> TX',
+    2: 'X -> +TX',
+    3: 'X -> ε',
+    4: 'T -> FY',
+    5: 'Y -> *FY',
+    6: 'Y -> ε',
+    7: 'F -> (S)',
+    8: 'F -> a',
+}
+print(grammar)
+
+START_SYMBOL = 'S'
+EPSILON = "ε"
+
+firstSets = {}
+followSets = {}
+terminals = []
+nonTerminals = []
+
+
+def startUp(grammar):
+    ll1.printGrammar(grammar)
+    ll1.buildFirstSets(grammar)
+    ll1.buildFollowSets(grammar)
+    printSet('First sets', firstSets)
+    printSet('Follow sets', followSets)
+    ll1.buildNonTerminals(grammar)
+    ll1.buildTerminals(grammar)
+   
+def printSet(type, sets ):
+    pass
---
+++ ll1.py
@@ -0,0 +1,17 @@
+def printGrammar(grammar):
+    pass
+
+def buildFirstSets(grammar):
+    pass
+
+def followSets(grammar):
+    pass
+
+def buildNonTerminals(grammar):
+    pass
+def buildTerminals(grammar):
+    pass
+def buildFollowSets(grammar):
+    pass
+
+

6c5e04f9727930cf7d1679f2ab9ecf802e969a90
Mateo A <mateoaguirre67@gmail.com>
2021-04-10 19:30:14 -0500

    Primeros

--- index.py
+++ index.py
@@ -1,34 +1,77 @@
-import ll1
+import LL1
 
 grammar = {
     1: 'S -> TX',
-    2: 'X -> +TX',
-    3: 'X -> ε',
-    4: 'T -> FY',
-    5: 'Y -> *FY',
-    6: 'Y -> ε',
-    7: 'F -> (S)',
-    8: 'F -> a',
+    2: 'X -> +TX | ε',
+    3: 'T -> FY',
+    4: 'Y -> *FY | ε',
+    5: 'F -> (S) | a'
 }
-print(grammar)
+# print(grammar)
 
-START_SYMBOL = 'S'
+OPERATOR_SYMBOL = '|'
 EPSILON = "ε"
 
 firstSets = {}
 followSets = {}
-terminals = []
+productions = []
 nonTerminals = []
+terminals = []
+splitGrammar = []
 
 
-def startUp(grammar):
+def startUp():
+    # ll1.buildFirstSets(grammar)
+    # ll1.buildFollowSets(grammar)
+    # printSet('First sets', firstSets)
+    # printSet('Follow sets', followSets)
+    # ll1.buildNonTerminals(grammar)
+    # ll1.buildTerminals(grammar)
+    # this.nonTerminals = buildNoTerminals()
+    splitingGrammar()
+    buildNoTerminals(splitGrammar)
+    buildProductions(splitGrammar)
+    buildTerminals()
+    LL1.firstOf(splitGrammar, terminals, nonTerminals)
 
-    ll1.buildFirstSets(grammar)
-    ll1.buildFollowSets(grammar)
-    printSet('First sets', firstSets)
-    printSet('Follow sets', followSets)
-    ll1.buildNonTerminals(grammar)
-    ll1.buildTerminals(grammar)
+def cleanNoTerminals():
+    for i in nonTerminals:
+        if i not in cleanNonTerminals:
+            cleanNonTerminals.append(i)
+    print("No Terminales")
+    print(cleanNonTerminals)
 
-def printSet(type, sets ):
+
+def splitingGrammar():
+    for value in grammar.values():
+        splitGrammar.append(value.replace(' ', '').split('->'))
+    print(splitGrammar)
+
+
+def buildNoTerminals(split):
+    cont = 0
+    for k in split:
+        nonTerminals.append(split[cont][0].replace(' ', ''))
+        cont = cont + 1
+    print("No Terminales")
+    print(nonTerminals)
+
+def buildProductions(split):
+    cont = 0
+    for k in split:
+        productions.append(split[cont][1].replace(' ', ''))
+        cont = cont + 1
+    print(productions)
+
+def buildTerminals():
+    for i in productions:
+        for c in range(len(i)):
+            if i[c] not in nonTerminals and i[c] != OPERATOR_SYMBOL and i[c] not in terminals:
+                terminals.append(i[c])
+    print(terminals)
+
+startUp()
+
+
+def printSet(type, sets):
     pass
--- ll1.py
+++ ll1.py
@@ -1,111 +1,21 @@
-import index
-import re
+firstSets = {}
 
 
-def buildFirstSets(grammar):
-    index.firstSets = {}
-    buildSet(firstOf)
+def firstOf(grammar, terminals, noTerminals):
+    for k in range(10):
+        for g in grammar:
+            temp = []
+            for i in range(len(g[1])):
+                if g[1][0] in terminals and g[1][0] not in temp:
+                    temp.append(g[1][0])
+                if g[1][i] == '|':
+                    if g[1][i+1] in terminals and g[1][i+1] not in temp:
+                        temp.append(g[1][i+1])
+                if g[1][0] in noTerminals and firstSets.get(g[1][0]) != temp:
+                    temp = firstSets.get(g[1][0])
+                if g[1][i] == '|':
+                    if g[1][i+1] in noTerminals and firstSets.get(g[1][i+1]) != temp:
+                        temp = (firstSets.get(g[1][i+1]))
+            firstSets[g[0]] = temp
 
-
-def followSets(grammar):
-    pass
-
-
-def buildNonTerminals(grammar):
-    pass
-
-
-def buildTerminals(grammar):
-    pass
-
-
-def buildFollowSets(grammar):
-    pass
-
-
-def isTerminal(symbol):
-    pass
-
-
-def buildSet(builder):
-    for k in index.grammar:
-        builder(index.grammar[k][0])
-
-
-def getProductionsForSymbol(symbol):
-    productionsForSymbol = {}
-    for k in index.grammar:
-        if index.grammar[k][0] == symbol:
-            productionsForSymbol[k] = index.grammar[k]
-
-    return productionsForSymbol
-
-
-def getLHS(production):
-    prod = []
-
-    for i in range(production):
-        if(production[i] == '-'):
-            break
-        prod.append(production[i])
-
-    return prod
-
-
-def getRHS(production):
-    prod = []
-    ban = False
-    for i in range(production):
-        if(ban):
-            prod.append(production[i])
-        else:
-            if(production[i] == '>'):
-                ban = True
-    return prod
-
-
-def merge(first, firstOfNonTerminal):
-    pass
-
-
-    
-
-
-def firstOf(symbol):
-    if index.firstSets[symbol]:
-        return index.firstSets[symbol]
-
-    first = index.firstSets[symbol] = {}
-
-    # If it's a terminal, its first set is just itself.
-
-    if (isTerminal(symbol)):
-        first[symbol] = True
-        return index.firstSets[symbol]
-    productionsForSymbol = getProductionsForSymbol(symbol)
-    for k in productionsForSymbol:
-
-        production = getRHS(productionsForSymbol[k])
-
-        for i in range(len(production)):
-            productionSymbol = production[i]
-            # Epsilon goes to the first set.
-            if productionSymbol == index.EPSILON:
-                first[index.EPSILON] = True
-                break
-
-            firstOfNonTerminal = firstOf(productionSymbol)
-
-            if not firstOfNonTerminal[index.EPSILON]:
-                merge(first, firstOfNonTerminal)
-                break
-
-            # Else (we got epsilon in the first non-terminal),
-
-            # - merge all except for epsilon
-            # - eliminate this non-terminal and advance to the next symbol
-            # (i.e. don't break this loop)
-            merge(first, firstOfNonTerminal, [index.EPSILON])
-            # don't break, go to the next `productionSymbol`.
-
-    return first
+    print(firstSets)

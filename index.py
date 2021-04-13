f1cc859f6cc86f5f3a283a8392e619fb66b19750
danydlpr <danieldelapavar@gmail.com>
2021-04-12 21:43:07 -0500

    conjunto prediccion y arreglo de bugs

--- index.py
+++ index.py
@@ -1,13 +1,10 @@
 import ll1
 
 grammar = {
-    1: 'S -> QA',
-    2: 'A -> oQA | ε',
-    3: 'Q -> RB',
-    4: 'B -> RB | ε ',
-    5: 'R -> F a | x | y',
-    6: 'F -> z'
+    1: 'E -> L A | i A | o E A | a E c A',
+    2: 'A -> o E A | s E A | d E p E A | ε'
 }
+
 
 
 # print(grammar)
@@ -24,24 +21,23 @@ splitGrammar = []
 
 
 def startUp():
-    # ll1.buildFirstSets(grammar)
-    # ll1.buildFollowSets(grammar)
-    # printSet('First sets', firstSets)
-    # printSet('Follow sets', followSets)
-    # ll1.buildNonTerminals(grammar)
-    # ll1.buildTerminals(grammar)
-    # this.nonTerminals = buildNoTerminals()
+    print(grammar)
     splitingGrammar()
     first_symbol = buildNoTerminals(splitGrammar)
     buildProductions(splitGrammar)
     buildTerminals()
     ll1.firstOf(splitGrammar, terminals, nonTerminals, OPERATOR_SYMBOL)
     ll1.nextOf(splitGrammar, terminals, nonTerminals, first_symbol, EPSILON)
+    ll1.predictionSet(splitGrammar, terminals, nonTerminals, EPSILON)
+    if ll1.isLL1():
+        print("Si es gramatica LL1")
+    else:
+        print("No es gramatica LL1")
+    
 
 def splitingGrammar():
     for value in grammar.values():
         splitGrammar.append(value.replace(' ', '').split('->'))
-    print(splitGrammar)
 
 
 def buildNoTerminals(split):
@@ -50,10 +46,6 @@ def buildNoTerminals(split):
         nonTerminals.append(split[cont][0].replace(' ', ''))
         cont = cont + 1
     first_symbol = nonTerminals[0]
-    print("Primer simbolo")
-    print(first_symbol)
-    print("No Terminales")
-    print(nonTerminals)
     return first_symbol
 
 def buildProductions(split):
@@ -61,17 +53,13 @@ def buildProductions(split):
     for k in split:
         productions.append(split[cont][1].replace(' ', ''))
         cont = cont + 1
-    print(productions)
 
 def buildTerminals():
     for i in productions:
         for c in range(len(i)):
             if i[c] not in nonTerminals and i[c] != OPERATOR_SYMBOL and i[c] not in terminals:
                 terminals.append(i[c])
-    print(terminals)
 
 startUp()
 
 
-def printSet(type, sets):
-    pass
--- ll1.py
+++ ll1.py
@@ -1,8 +1,8 @@
 firstSets = {}
 nextSets = {}
-
+predictionSets = {}
 def firstOf(grammar, terminals, noTerminals, OPERATOR_SYMBOL):
-    for k in range(5):
+    for k in range(10):
         for g in grammar:
             temp = []
             for i in range(len(g[1])):
@@ -14,15 +14,12 @@ def firstOf(grammar, terminals, noTerminals, OPERATOR_SYMBOL):
                     if g[1][i+1] in noTerminals and firstSets.get(g[1][i+1]) != temp:
                         temp = (firstSets.get(g[1][i+1]))
                 if g[1][0] in noTerminals and firstSets.get(g[1][0]) != temp:
+                    temp = []
                     temp = firstSets.get(g[1][0])
-                    print(g[1][0])
-                    print(temp)
                 if temp is None:
                     temp = []
             firstSets[g[0]] = temp
-            #print(g[0])
-            #print(firstSets[g[0]])
-    print(grammar)
+    print('Primeros')
     print(firstSets)
 
 def nextOf(grammar, terminals, noTerminals, first_symbol, EPSILON):
@@ -34,7 +31,7 @@ def nextOf(grammar, terminals, noTerminals, first_symbol, EPSILON):
                     try:
                         if g[0] == first_symbol and '$' not in temp:
                                 temp.append('$')
-                        if g2[1][i] == first_symbol and g2[1][i+1] in terminals:
+                        if g2[1][i] == first_symbol and g2[1][i+1] in terminals and g2[1][i+1] not in temp:
                             temp.append(g2[1][i+1])
                         if g[0] == g2[1][i] and g2[1][i+1] in terminals and g2[1][i+1] not in temp:
                             temp.append(g2[1][i+1])
@@ -43,12 +40,51 @@ def nextOf(grammar, terminals, noTerminals, first_symbol, EPSILON):
                                 temp = firstSets.get(g2[1][i+1]) 
                                 if EPSILON in temp:
                                     temp.remove(EPSILON)
-                                    temp += nextSets.get(g2[0])
-                            if g2[1][i+1] == '|' and g2[0] != g2[1][i]:
-                                temp = nextSets.get(g2[0])
+                                    if str(temp).strip("[]") not in str(nextSets.get(g2[0])).strip("[]"):
+                                        temp += nextSets.get(g2[0])
+                            if g2[1][i+1] == '|' and g2[0] != g2[1][i] and str(nextSets.get(g2[0])).strip("[]") not in str(temp).strip("[]"):
+                                temp += nextSets.get(g2[0])
+                        if temp is None:
+                            temp = []
                     except: 
-                        temp = nextSets.get(g2[0])
-            
+                        try:
+                            for i in nextSets.get(g2[0]):
+                                if i not in temp:
+                                    temp.extend(i)
+                        except:
+                            continue
             nextSets[g[0]] = temp
-            
+    print('Siguientes')
     print(nextSets)
+
+def predictionSet(grammar, terminals, noTerminals, EPSILON):
+    for g in grammar:
+        temp = g[1].split('|')
+        temp3 = []
+        for t in temp:
+            if t[0] in terminals:
+                temp3 += t[0]
+            if t[0] in noTerminals:
+                temp3 += firstSets.get(t[0])
+                if EPSILON in temp3:
+                    temp3.remove(EPSILON)
+                    temp3 += nextSets.get(g[0])
+            if t[0] == EPSILON:
+                temp3 += nextSets.get(g[0])
+                temp3.remove(EPSILON)
+        predictionSets[g[0]] = temp3
+    print("Conjunto prediccion")
+    print(predictionSets)
+
+def isLL1():
+    for p in predictionSets:
+        if not isLL1count(predictionSets.get(p)):
+            return False
+    return True
+
+def isLL1count(p):
+    for i in p:
+        cont = p.count(i)
+        if cont > 1:
+            return False
+    return True
